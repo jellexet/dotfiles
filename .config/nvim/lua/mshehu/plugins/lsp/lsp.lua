e2e6e884a9f50ed06e85617c782aefd86a2edd3c
jellexet <mario.shehu.07@gmail.com>
2025-11-30 22:55:40 +0100

    [nvim] Added rust and bash lsp configurations

--- .config/nvim/lua/mshehu/plugins/lsp/lsp.lua
+++ .config/nvim/lua/mshehu/plugins/lsp/lsp.lua
@@ -13,11 +13,24 @@ return {
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
         local clangd_config = require("mshehu.lspconfigs.clangd")
-        vim.lsp.config("clangd", clangd_config)
+        local rust_analyzer_config = require("mshehu.lspconfigs.rust_analyzer")
 
+        vim.lsp.config("clangd", clangd_config)
+        vim.lsp.config("rust_analyzer", rust_analyzer_config)
+        vim.lsp.config.bashls = {
+            cmd = { "bash-language-server", "start" },
+            filetypes = { "bash", "sh", "zsh" },
+            root_markers = { ".git" },
+            settings = {
+                bashIde = {
+                    globPattern = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command)",
+                },
+            },
+        }
         vim.lsp.config("*", {
             capabilities = capabilities,
         })
+
         vim.lsp.enable({
             "bashls",
             "clangd",
